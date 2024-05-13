bcaf277b177b075c246c1817b9284f5875858ca7
Hussien Essam <112499614+flavasava2022@users.noreply.github.com>
2024-05-13 18:03:59 +0300

    update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,20 @@
 module.exports = [
+  {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+              'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
